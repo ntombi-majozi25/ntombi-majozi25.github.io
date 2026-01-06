3ce0a7dc6c1f73acdad6308450e0969d2d137679
MAJOZI N S MISS <19902115@mylife.unisa.ac.za>
2026-01-06 15:06:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const reveals=document.querySelectorAll('.reveal');
+
+const observer=new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry =>{
+            if (entry.isIntersecting){
+                entry.target.classList.add('active');
+            } else {entry.target.classList.remove('active');
+                   }
+        });
+    },
+    {threshhold: 0.2}
+);
+reveals.forEach(el => observer.observe(el));
